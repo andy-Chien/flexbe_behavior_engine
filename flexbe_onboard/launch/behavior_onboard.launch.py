b3ab89da6c564100b5dafe98b454d6bec76dff9a
Andy Chien <r960411@gmail.com>
2023-06-30 00:49:21 +0800

    add namespace

--- flexbe_onboard/launch/behavior_onboard.launch.py
+++ flexbe_onboard/launch/behavior_onboard.launch.py
@@ -9,6 +9,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true') # For simulations
 
     return LaunchDescription([
+        DeclareLaunchArgument("ns", default_value=""),
         DeclareLaunchArgument("log_enabled", default_value="False"),
         DeclareLaunchArgument("log_folder", default_value="~/.flexbe_logs"),
         DeclareLaunchArgument("log_serialize", default_value="yaml"),
@@ -17,7 +18,8 @@ def generate_launch_description():
         DeclareLaunchArgument("enable_clear_imports", default_value="False",
             description="Delete behavior-specific module imports after execution."),
         Node(
-            name="behavior", package="flexbe_onboard", executable="start_behavior", output="screen",
+            name="behavior", package="flexbe_onboard", executable="start_behavior",
+            namespace=LaunchConfiguration("ns"), output="screen",
             parameters=[{"log_enabled": LaunchConfiguration("log_enabled"),
                          "log_folder": LaunchConfiguration("log_folder"),
                          "log_serialize": LaunchConfiguration("log_serialize"),
